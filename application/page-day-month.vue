bc02ca3b921941c889907d5049488643a16cb838
Hiswe <hiswehalya@gmail.com>
2018-06-02 16:21:00 +0700

    DAYS & MONTHS – highliht current date

--- application/page-day-month.vue
+++ application/page-day-month.vue
@@ -3,24 +3,29 @@
     <h1>Days & Months</h1>
     <h2>days</h2>
     <ul>
-      <li v-for="(day, index) in days" :key="day.rtgs">
-        {{ index }}
-        {{ day.en }}
-        {{ day.th }}
-        {{ day.rtgs }}
+      <li v-for="(day, index) in days" :key="day.rtgs" :class="{ active: currentDay === index + 1 }">
+        <!-- {{ index }} -->
+        <span>{{ day.en }}</span>
+        <span>{{ day.th }} <small>{{ day.rtgs }}</small>  </span>
       </li>
     </ul>
     <h2>months</h2>
     <ul>
-      <li v-for="(month, index) in months" :key="month.rtgs">
-        {{ index }}
-        {{ month.en }}
-        {{ month.th }}
-        {{ month.rtgs }}
+      <li v-for="(month, index) in months" :key="month.rtgs" :class="{ active: currentMonth  === index + 1}">
+        <!-- {{ index }} -->
+        <span>{{ month.en }}</span>
+        <span>{{ month.th }} <small>{{ month.rtgs }}</small>  </span>
       </li>
     </ul>
   </section>
 </template>
+
+<style scoped>
+.active {
+  color: red;
+}
+</style>
+
 
 <script>
 import { DateTime } from 'luxon'
