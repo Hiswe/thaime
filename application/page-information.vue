622a43850e6dd3235a2fc2e36541c91064860f3b
Hiswe <hiswehalya@gmail.com>
2018-06-09 12:07:06 +0700

    INFORMATIONS – regular title

--- application/page-information.vue
+++ application/page-information.vue
@@ -1,8 +1,21 @@
 <template>
   <section class="page-content">
-    <h1>informations</h1>
+    <t-page-title>
+      informations
+    </t-page-title>
     <div>
       <a href="http://thai-language.com/">thai-language.com</a>
     </div>
   </section>
 </template>
+
+<script>
+import pageTitle from './ui/page-title'
+export default {
+  name: `page-information`,
+  components: {
+    't-page-title': pageTitle,
+  },
+}
+</script>
+
