f231cfe1c5451d40f985f5e07512434f28bef651
Hiswe <hiswehalya@gmail.com>
2018-06-11 12:19:54 +0700

    APP – add the logo

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@
 const path = require('path')
 const gulp = require('gulp')
 const $ = require('gulp-load-plugins')()
+const args = require(`yargs`).argv
+
+const isRelease = args.release === true
+const destFolder = isRelease ? `public` : `dist`
 
 ////////
 // ICONS
@@ -48,3 +52,39 @@ exports.icons = () => {
     .pipe($.if(/[.]vue$/, gulp.dest(`application/ui`)))
     .pipe($.if(/[.]html$/, gulp.dest(`source/icons`)))
 }
+
+////////
+// APP LOGO
+////////
+
+const logoBasename = `touch-icon`
+
+const appLogo = () => {
+  return gulp
+    .src(`source/thaime-logo.png`)
+    .pipe($.imageResize({ width: 192, height: 192, upscale: true }))
+    .pipe(
+      $.rename(path => (path.basename = `launcher-icon-${logoBasename}-4x`))
+    )
+    .pipe(gulp.dest(destFolder))
+    .pipe($.imageResize({ width: 180, height: 180, upscale: true }))
+    .pipe($.rename(path => (path.basename = `${logoBasename}-iphone-6-plus`)))
+    .pipe(gulp.dest(destFolder))
+    .pipe($.imageResize({ width: 152, height: 152, upscale: true }))
+    .pipe($.rename(path => (path.basename = `${logoBasename}-ipad-retina`)))
+    .pipe(gulp.dest(destFolder))
+    .pipe($.imageResize({ width: 144, height: 144, upscale: true }))
+    .pipe($.rename(path => (path.basename = `${logoBasename}-web-app`)))
+    .pipe(gulp.dest(destFolder))
+    .pipe($.imageResize({ width: 120, height: 120, upscale: true }))
+    .pipe($.rename(path => (path.basename = `${logoBasename}-iphone-retina`)))
+    .pipe(gulp.dest(destFolder))
+    .pipe($.imageResize({ width: 76, height: 76, upscale: true }))
+    .pipe($.rename(path => (path.basename = `${logoBasename}-icon-ipad`)))
+    .pipe(gulp.dest(destFolder))
+    .pipe($.imageResize({ width: 57, height: 57, upscale: true }))
+    .pipe($.rename(path => (path.basename = `${logoBasename}-icon-iphone`)))
+    .pipe(gulp.dest(destFolder))
+}
+appLogo.description = `resize favicon for different devices`
+exports[`app-logo`] = appLogo
