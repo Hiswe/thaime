fb66ed4caeedc22a97180aaccb5200a6f22776d2
Hiswe <hiswehalya@gmail.com>
2018-06-12 20:03:03 +0700

    RELEASE – create a release branch

--- bin/release.js
+++ bin/release.js
@@ -21,7 +21,7 @@ initRelease()
 
 async function initRelease() {
   ////////
-  // BUMP
+  // BUMP CONFIGURATION
   ////////
 
   const bumping = await inquirer.prompt([
@@ -77,131 +77,105 @@ async function initRelease() {
   }
   shell.echo(`…deployed!`)
 
+  if (!bumping.bump) {
+    shell.echo(`Skipping pushing tag`)
+    shell.exit(0)
+  }
+
   ////////
   // TAG
   ////////
 
+  //----- BRANCH CONTROL
+
+  const currentBranch = shell.exec(`git branch`, { silent: true }).grep(/^\*/)
+  const branchName = currentBranch.stdout.replace(/[\*\n\s]/g, ``)
+
+  if (branchName !== `master`) {
+    shell.echo(`Sorry, you need to be on the master branch`)
+    shell.exit(1)
+  }
+
+  //----- INITIALIZING FOLDERS & HELPERS
+
+  const originalDir = shell.pwd()
+  const copyDir = shell.exec(`mktemp -d /tmp/thaime.XXX`, { silent: true })
+  // stdout come with a line break. Remove it for better path joining
+  const copyDirPath = copyDir.stdout.replace(`\n`, ``)
+
+  const teardown = () => {
+    shell.cd(originalDir)
+    shell.rm(`-Rf`, copyDir)
+  }
+
+  shell.echo(`temp dir will be created at: ${copyDirPath}`)
+
+  //-----  COPYING FILES TO TMP
+
+  shell.echo(`begin copy…`)
+
+  shell.echo(`…git files…`)
+  shell.cp(`-r`, `./.git/.`, path.join(copyDirPath, `/.git`))
+
+  shell.echo(`…dist files…`)
+  shell.cp(`-r`, `./dist/*`, copyDir)
+  shell.echo(`…copy end`)
+  shell.cd(copyDir)
+
+  //----- SETTING A NEW BRANCH
+
+  const tmpBranchName = `${BRANCH}-${bumping.version}`
+  shell.echo(`checking out “${tmpBranchName}” branch`)
+
+  // orphan branch for having a clean new branch
+  const gitCheckout = shell.exec(`git checkout --orphan ${tmpBranchName} `, {
+    silent: true,
+  })
+  if (gitCheckout.code !== 0) {
+    shell.echo(`Unable to checkout`)
+    shell.echo(gitCheckout.stderr)
+    teardown()
+    shell.exit(1)
+  }
+
+  //----- ADDING THE FILES
+
+  shell.exec(`git add .`, { silent: true })
+  shell.exec(`git commit -m "RELEASE – version ${bumping.version}"`, {
+    silent: true,
+  })
+
+  //----- PUSHING THE FILES
+
+  shell.echo(`pushing to “${BRANCH}” branch…`)
+  const ghPagePush = shell.exec(
+    `git push origin ${tmpBranchName}:${BRANCH} --force`,
+    { silent: true }
+  )
+  if (ghPagePush.code !== 0) {
+    shell.echo(`Error: Git push failed`)
+    shell.echo(ghPagePush.stderr)
+    teardown()
+    shell.exit(1)
+  } else {
+    shell.echo(`…push done!`)
+  }
+
+  //----- TAGGING THE VERSION
+
+  shell.echo(`tagging version…`)
+  shell.exec(`git tag v${bumping.version}`, { silent: true })
+  const tagPush = shell.exec(`git push --tags`, { silent: true })
+  if (tagPush.code !== 0) {
+    shell.echo(`Error: Git tag push failed`)
+    shell.echo(tagPush.stderr)
+    teardown()
+    shell.exit(1)
+  } else {
+    shell.echo(`…tag push done!`)
+  }
+
+  //----- TEARDOWN
+  teardown()
   shell.exit(0)
 }
-
-// const currentBranch = shell.exec(`git branch`, { silent: true }).grep(/^\*/)
-// const branchName = currentBranch.stdout.replace(/[\*\n\s]/g, ``)
-
-// if (branchName !== `master`) {
-//   shell.echo(`Sorry, you need to be on the master branch`)
-//   shell.exit(1)
-// }
-
-// ////////
-// // DEPLOYING TO FIREBASE
-// ////////
-
-// if (!bc.isGhRelease) {
-//   shell.echo(`Deploying to firebase…`)
-//   const firebaseDeploy = shell.exec(`yarn run firebase deploy`, {
-//     silent: true,
-//   })
-//   if (firebaseDeploy.code !== 0) {
-//     shell.echo(`Unable deploy to firebase`)
-//     shell.echo(firebaseDeploy.stderr)
-//     shell.exit(1)
-//   }
-//   shell.echo(`…deployed`)
-// }
-
-// ////////
-// // INITIALIZING FOLDERS & HELPERS
-// ////////
-
-// const originalDir = shell.pwd()
-// const copydir = shell.exec(`mktemp -d /tmp/thailpha.XXX`, { silent: true })
-// // stdout come with a linebreak. Remove it for better path joining
-// const copydirPath = copydir.stdout.replace(`\n`, ``)
-
-// const teardown = () => {
-//   shell.cd(originalDir)
-//   shell.rm(`-Rf`, copydir)
-// }
-
-// shell.echo(`temp dir will be created at: ${copydirPath}`)
-
-// ////////
-// // COPYING FILES TO TMP
-// ////////
-
-// shell.echo(`begin copy…`)
-
-// //----- GIT FILES
-
-// shell.echo(`…git files…`)
-// shell.cp(`-r`, `./.git/.`, path.join(copydirPath, `/.git`))
-
-// //----- DIST FILES
-
-// shell.echo(`…dist files…`)
-// shell.cp(`-r`, `./dist/*`, copydir)
-// shell.echo(`…copy end`)
-// shell.cd(copydir)
-
-// ////////
-// // GIT
-// ////////
-
-// //----- SETTING A NEW BRANCH
-
-// const tmpBranchName = `${BRANCH}-${version}`
-// shell.echo(`checking out “${tmpBranchName}” branch`)
-
-// // orphan branch for having a clean new branch
-// const gitCheckout = shell.exec(`git checkout --orphan ${tmpBranchName} `, {
-//   silent: true,
-// })
-// if (gitCheckout.code !== 0) {
-//   shell.echo(`Unable to checkout`)
-//   shell.echo(gitCheckout.stderr)
-//   teardown()
-//   shell.exit(1)
-// }
-
-// //----- ADDING THE FILES
-
-// shell.exec(`git add .`, { silent: true })
-// shell.exec(`git commit -m "RELEASE – version ${version}"`, { silent: true })
-
-// //----- PUSHING THE FILES
-
-// shell.echo(`pushing to “${BRANCH}” branch…`)
-// const ghPagePush = shell.exec(
-//   `git push origin ${tmpBranchName}:${BRANCH} --force`,
-//   { silent: true }
-// )
-// if (ghPagePush.code !== 0) {
-//   shell.echo(`Error: Git push failed`)
-//   shell.echo(ghPagePush.stderr)
-//   teardown()
-//   shell.exit(1)
-// } else {
-//   shell.echo(`…push done!`)
-// }
-
-// //----- TAGGING THE VERSION
-
-// if (bc.skipBump) {
-//   shell.echo(`Skipping pushing tag`)
-//   shell.exit(0)
-// }
-
-// shell.echo(`tagging version…`)
-// shell.exec(`git tag v${version}`, { silent: true })
-// const tagPush = shell.exec(`git push --tags`, { silent: true })
-// if (tagPush.code !== 0) {
-//   shell.echo(`Error: Git tag push failed`)
-//   shell.echo(tagPush.stderr)
-//   teardown()
-//   shell.exit(1)
-// } else {
-//   shell.echo(`…tag push done!`)
-// }
-
-// // TEARDOW
-// teardown()
